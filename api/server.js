593201134026deb74f227c3af5d80c1194b40a4d
Lambda_School_Loaner_112 <lambda_school_loaner_112@Lambda-School-Loaner-112s-MacBook-Pro.local>
2019-09-19 17:26:42 -0500

    MVP Day2

--- api/server.js
+++ api/server.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session')
 const bcrypt = require('bcryptjs');
-const Users = require('../users/users-model.js');
 
+const Users = require('../users/users-model.js');
+const restricted = require('./restricted-middleware');
 const server = express();
 
+const sessionConfig = {
+  name: 'honolulu',
+  secret: process.send.SESSION_SECRET || 'I swear I am up to no good',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 
 server.get('/', (req, res) => {
-  res.send("Webauth-i-challenge!");
-});//endpoint works
+  res.send("Webauth-i-challenge & Webauth-ii-challenge");
+});
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
@@ -29,7 +43,7 @@ console.log(user)
     .catch(error => {
       res.status(500).json(error);
     });
-}); //endpoint works
+}); 
 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
@@ -38,6 +52,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -46,15 +61,15 @@ server.post('/api/login', (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
-}); //endpoint works
+}); 
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
-}); //endpoint works
+}); 
 
 server.get('/hash', (req, res) => {
   const name = req.query.name;
